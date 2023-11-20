1d7674657496b24087be934a44f8b4b281d42bc5
Susan Willa Roby <swroby@crimson.ua.edu>
2023-11-19 21:32:08 -0600

    booth confirmation page to show after I figure out how to pull available booths from database

--- client/hostabooth.js
+++ client/hostabooth.js
@@ -1,84 +1,144 @@
-async function GetBusinessPerDate(id, date) {
-    localStorage.setItem('pickedDate', date);
-    console.log(id);
-    console.log(date);
-    let businesses = await GetAllBusinesses();
-    let attendsplural = await GetAllAttends();
-  
+async function handleOnLoad() {
+  await CreateTable();
+}
+
+async function GetAllAttends() {
+  try {
+    let response = await fetch('http://localhost:5124/api/attends');
+    attends = await response.json();
+    return attends;
+  } catch (error) {
+    console.error("Error fetching attends:", error);
+    return [];
+  }
+}
+
+async function GetAllEvents() {
+  try {
+    let response = await fetch('http://localhost:5124/api/event');
+    events = await response.json();
+    return events;
+  } catch (error) {
+    console.error("Error fetching events:", error);
+    return [];
+  }
+}
+
+async function CreateTable() {
+  try {
+    let events = await GetAllEvents();
     let html = `
       <div class="container my-5 text-center">
-        <h1 class="display-5 fw-bold">Businesses Attending ${date}</h1>
+        <h1 class="display-5 fw-bold">Upcoming Events</h1>
         <div class="p-5 text-center rounded-3" style="background-color: hsl(0, 0%, 100%);">
           <table class="table table-hover center" id="myTable">
             <thead id="table-header">
               <tr>
                 <th scope="col">Event ID</th>
-                <th scope="col">Booth Location</th>
-                <th scope="col">Company Name</th>
-                <th scope="col">Product Type</th>
-                <th scope="col">Logo</th>
+                <th scope="col">Date</th>
+                <th scope="col">Location</th>
+                <th scope="col">Select</th>
               </tr>
             </thead>
             <tbody id="myTableBody" class="table-group-divider">`;
-  
-    attendsplural.forEach(function (a) {
-      if (a.eventID == id) {
-        // Check if the booth has been selected for this event
-        if (isBoothSelected(a.BoothLocation)) {
-          html += `
-            <tr>
-              <td>${a.eventID}</td>
-              <td>${a.  BoothLocation}</td>
-              <td>${businesses[a.businessID - 1].companyName}</td>
-              <td>${businesses[a.businessID - 1].productType}</td>
-              <td><img src="${businesses[a.businessID - 1].logo}" alt="logo failed" style="max-width: 100px; max-height: 100px;"></td>
-            </tr>`;
-        }
-      }
-    });
-  
-    html += `</tbody>
-      </table>
-    </div>
-  </div>
-  <div class="center-container">
-    <button class="btn btn-primary my-5 ms-5">
-      <a class="nav-link active" aria-current="page" href="purchasetickets2.html">SUBMIT</a>
-    </button>
-  </div>`;
-    document.getElementById('businesses-at-market-table').innerHTML = html;
-  
-    const pickedDate = localStorage.getItem('pickedDate');
-    console.log(pickedDate);
-  }
-  
-  function isBoothSelected(boothLocation) {
-    // Implement logic to check if the booth has been selected based on the database
-    // You may need to fetch data from the database or use a different approach depending on your backend setup.
-    // For the sake of this example, assuming a global array to store selected booth locations.
-    const selectedBooths = ['Booth1', 'Booth3', 'Booth5']; // Replace with your actual data
-  
-    return selectedBooths.includes(boothLocation);
-  }
-  
 
-// $(document).ready(function() {
-//     var city, map;
-//     map = $('.ct-map');
-//     city = map.find('.ct-city');
-//     city.each(function() {
-//       var button, that;
-//       that = $(this);
-//       button = that.find('.ct-city__button');
-//       return button.on('click', function() {
-//         city.not(that).removeClass('active');
-//         if (that.hasClass('active')) {
-//           that.removeClass('active');
-//           return map.removeClass('popup-open');
-//         } else {
-//           that.addClass('active');
-//           return map.addClass('popup-open');
-//         }
-//       });
-//     });
-//   });
+    events.forEach(function (e) {
+      html += `
+        <tr>
+          <td>${e.eventID}</td>
+          <td>${e.date}</td>
+          <td>${e.location}</td>
+          <td>
+            <button class="btn btn-primary" onclick="SelectEvent('${e.eventID}')">Select</button>
+          </td>
+        </tr>`;
+    });
+
+    html += `</tbody>
+          </table>
+        </div>
+      </div>`;
+    document.getElementById('events-table').innerHTML = html;
+  } catch (error) {
+    console.error("Error creating table:", error);
+  }
+}
+
+async function SelectEvent(id) {
+  try {
+    let availableBooths = await GetAvailableBooths(id);
+
+    let html = `
+      <div class="container">
+        <img src="./resources/styles/Map_2017_2-e1588350571960.jpg" alt="failed to load">
+        <div>
+          <div class="dropdown text-center">
+            <ul class="dropdown-menu" id="dropdown-container" style="max-height: 300px; overflow-y: auto;">`;
+
+    availableBooths.forEach(function (booth) {
+      html += `<div class="container"><li><button class="btn btn-light" onclick="GetBusinessPerBooth('${booth.eventID}', '${booth.boothLocation}')">${booth.boothLocation}</button></li></div>`;
+    });
+
+    html += `</ul>
+          </div>
+        </div>
+      </div>`;
+
+    document.getElementById('choose-booth').innerHTML = html;
+  } catch (error) {
+    console.error("Error fetching available booths:", error);
+  }
+}
+
+async function GetAvailableBooths(eventID) {
+  try {
+    let allBooths = await GetAllBoothsForEvent(eventID);
+    let selectedBooths = await GetSelectedBooths(eventID);
+    let availableBooths = allBooths.filter(booth => !selectedBooths.includes(booth.boothLocation));
+    return availableBooths;
+  } catch (error) {
+    console.error("Error fetching available booths:", error);
+    return [];
+  }
+}
+
+async function GetAllBoothsForEvent(eventID) {
+  try {
+    const response = await fetch(`http://localhost:5124/api/attends?eventID=${eventID}`);
+    const data = await response.json();
+    return data; // Assuming data is an array of booths for the given event
+  } catch (error) {
+    console.error("Error fetching booths for the event:", error);
+    return [];
+  }
+}
+
+async function GetSelectedBooths(eventID) {
+  try {
+    const response = await fetch(`http://localhost:5124/api/getSelectedBooths?eventID=${eventID}`);
+    const selectedBooths = await response.json();
+    return selectedBooths;
+  } catch (error) {
+    console.error(`Error fetching selected booths for event ${eventID}:`, error);
+    return [];
+  }
+}
+
+async function BookBooth(eventID) {
+  try {
+    // Implement the logic to book a booth (You may need to send a request to your server to update the database)
+
+    // Assuming you have a function to update the database, replace the following line with the actual logic
+    await UpdateBoothBooking(eventID);
+
+    // Redirect to the confirmation page
+    window.location.href = 'businessboothconfirmation.html'; // Change 'confirmation.html' to the actual page you want to redirect to
+  } catch (error) {
+    console.error(`Error booking booth for event ID ${eventID}:`, error);
+    // Handle error, display a message, or redirect to an error page
+  }
+}
+
+
+
+
