dee00f47743b5509a07e3f68ed9de048bd5dda12
Carson Miller <clmiller16@crimson.ua.edu>
2023-11-29 14:19:26 -0600

    env file

--- api/database/Database.cs
+++ api/database/Database.cs
@@ -8,23 +8,7 @@ namespace api.database
 {
     public class Database
     {
-        // public string host{get;set;}
-        // public string database{get;set;}
-        // public string username{get;set;}
-        // public string port{get;set;}
-        // public string password{get;set;}
-
         public string cs {get; set;}
-
-        // public Database(){
-        //     host = "ckshdphy86qnz0bj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-        //     database = "v8nyhpcxiv4yp89x";
-        //     username = "wuuxnhs9tp9xm5xn";
-        //     port = "3306";
-        //     password = "qhefqssrcg0jyw5t";
-
-        //     cs = $"server={host};user={username};database={database};port={port};password={password}";
-        // }
 
         public Database(){
             DotNetEnv.Env.Load();
